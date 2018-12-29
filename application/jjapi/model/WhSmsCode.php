a855ac1b3164181c8003397b8eb7eb3e7dfd1335
jiao <1114351965@qq.com>
2018-12-29 11:13:31 +0800

    gai11

--- application/jjapi/model/WhSmsCode.php
+++ application/jjapi/model/WhSmsCode.php
@@ -17,18 +17,18 @@ class WhSmsCode extends BaseModel
 
     public static function checkByMobile($mobile,$type)
     {
-        $mobile_count = self::whereTime('create_time', 'today')->where(['mobile_number' => $mobile, 'type' => $type])->count();
+        $mobile_count = self::whereTime('create_time', 'today')->where(['mobile' => $mobile, 'type' => $type])->count();
         return $mobile_count;
     }
 
     public static function checkCode($mobile, $code, $type)
     {
-        $codeInfo = self::where(['mobile_number' => $mobile, 'validate_code' => $code, 'type' => $type])->order('id', 'desc')->limit(1)->find();
+        $codeInfo = self::where(['mobile' => $mobile, 'validate_code' => $code, 'type' => $type])->order('id', 'desc')->limit(1)->find();
         return $codeInfo;
     }
 
     public static function changeStatus($mobile, $code, $type, $time = '')
     {
-        self::where(['mobile_number'=>$mobile, 'validate_code'=>$code, 'type'=>$type])->order('id', 'desc')->limit(1)->update(['using_time'=>$time]);
+        self::where(['mobile'=>$mobile, 'validate_code'=>$code, 'type'=>$type])->order('id', 'desc')->limit(1)->update(['using_time'=>$time]);
     }
 }
