c6ca2c852ffb3f8a341dd239e4a76668576206b8
Devkeep <363927173@qq.com>
2018-12-30 11:49:04 +0800

    pay

--- application/jjapi/controller/v1/Pay.php
+++ application/jjapi/controller/v1/Pay.php
@@ -29,6 +29,7 @@ class Pay extends Controller
         return json([
             'a' => 2
         ]);
+        
         // v($_POST);
         // var_dump($_FILES);exit;
         $postData = Request::instance()->post();
