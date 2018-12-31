757158dbbf06593c8a0893ec81ec2cdd9cd91b4e
jiao <1114351965@qq.com>
2018-12-31 13:06:56 +0800

    adf

--- application/jjapi/model/WhBanner.php
+++ application/jjapi/model/WhBanner.php
@@ -11,6 +11,11 @@ namespace app\jjapi\model;
 
 class WhBanner extends BaseModel
 {
+    public function getImgUrlAttr($value)
+    {
+        return config('setting.domain').$value;
+    }
+
     public static function getBanner($type)
     {
         return self::where('type', '=', $type)->order('sort_order', 'asc')->select();
