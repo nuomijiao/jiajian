6606ff6e9ebd2d2d035287ee61f46b2ba5dab139
jiao <1114351965@qq.com>
2019-01-16 18:19:21 +0800

    13

--- application/extra/aliyun.php
+++ application/extra/aliyun.php
@@ -28,4 +28,5 @@ return [
 
 
     'alipay_public_key' => 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCszzcP86t02+2k3IkpCgM9KE2QVKCXX0AeYYxlLyEQ7rWeCjRPH1bDJm+sNnyJfEOfYWxkrS0ICfl52o5nkWdOIC90CgSaTTX5durNRluOy4zyIJFtklt/ZD9DTnau7mBr3Tnb/uzMk3+pqzW+F/iVriFhlzpqSMX7U8I+pcdJMwIDAQAB',
+    'sign_type' => 'RSA'
 ];
