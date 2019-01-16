850c6fe33377d2f3cf6ddbb2e264a6b9441a45b4
jiao <1114351965@qq.com>
2019-01-16 18:42:33 +0800

    123

--- application/extra/aliyun.php
+++ application/extra/aliyun.php
@@ -29,6 +29,6 @@ return [
 
     'alipay_public_key' => 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCszzcP86t02+2k3IkpCgM9KE2QVKCXX0AeYYxlLyEQ7rWeCjRPH1bDJm+sNnyJfEOfYWxkrS0ICfl52o5nkWdOIC90CgSaTTX5durNRluOy4zyIJFtklt/ZD9DTnau7mBr3Tnb/uzMk3+pqzW+F/iVriFhlzpqSMX7U8I+pcdJMwIDAQAB',
     'sign_type' => 'RSA',
-    'transport' => 'http',
+    'transport' => 'https',
     'partner' => '2088431169714534',
 ];
--- extend/AliPay/alipay_notify.class.php
+++ extend/AliPay/alipay_notify.class.php
@@ -45,8 +45,8 @@ class AlipayNotify {
 			//生成签名结果
 			$isSign = $this->getSignVeryfy($_POST, $_POST["sign"]);
 			//获取支付宝远程服务器ATN结果（验证是否是支付宝发来的消息）
-			$responseTxt = 'false';
-			if (! empty($_POST["notify_id"])) {$responseTxt = $this->getResponse($_POST["notify_id"]);}
+//			$responseTxt = 'false';
+//			if (! empty($_POST["notify_id"])) {$responseTxt = $this->getResponse($_POST["notify_id"]);}
 			
 			//写日志记录
 			//if ($isSign) {
@@ -62,11 +62,16 @@ class AlipayNotify {
 			//验证
 			//$responsetTxt的结果不是true，与服务器设置问题、合作身份者ID、notify_id一分钟失效有关
 			//isSign的结果不是true，与安全校验码、请求时的参数格式（如：带自定义参数等）、编码格式有关
-			if (preg_match("/true$/i",$responseTxt) && $isSign) {
-				return true;
-			} else {
-				return false;
-			}
+//			if (preg_match("/true$/i",$responseTxt) && $isSign) {
+//				return true;
+//			} else {
+//				return false;
+//			}
+            if ($isSign) {
+                return true;
+            } else {
+                return false;
+            }
 		}
 	}
 	
