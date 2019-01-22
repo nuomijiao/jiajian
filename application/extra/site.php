b229c14d6f99599676eb4611f2d8b9a506e826bf
jiao <1114351965@qq.com>
2019-01-22 14:17:47 +0800

    3

--- application/extra/site.php
+++ application/extra/site.php
@@ -1,38 +1,43 @@
 <?php
 
 return array (
-  'name' => 'FastAdmin',
-  'beian' => '',
-  'cdnurl' => '',
-  'version' => '1.0.1',
-  'timezone' => 'Asia/Shanghai',
-  'forbiddenip' => '',
-  'languages' => 
-  array (
-    'backend' => 'zh-cn',
-    'frontend' => 'zh-cn',
-  ),
-  'fixedpage' => 'dashboard',
-  'categorytype' => 
-  array (
-    'default' => 'Default',
-    'page' => 'Page',
-    'article' => 'Article',
-    'test' => 'Test',
-  ),
-  'configgroup' => 
-  array (
-    'basic' => 'Basic',
-    'email' => 'Email',
-    'dictionary' => 'Dictionary',
-    'user' => 'User',
-    'example' => 'Example',
-  ),
-  'mail_type' => '1',
-  'mail_smtp_host' => 'smtp.qq.com',
-  'mail_smtp_port' => '465',
-  'mail_smtp_user' => '10000',
-  'mail_smtp_pass' => 'password',
-  'mail_verify_type' => '2',
-  'mail_from' => '10000@qq.com',
+    'name' => '加减数据',
+    'beian' => '',
+    'cdnurl' => '',
+    'version' => '1.0.1',
+    'timezone' => 'Asia/Shanghai',
+    'forbiddenip' => '',
+    'languages' =>
+        array (
+            'backend' => 'zh-cn',
+            'frontend' => 'zh-cn',
+        ),
+    'fixedpage' => 'dashboard',
+    'categorytype' =>
+        array (
+            'default' => 'Default',
+            'page' => 'Page',
+            'article' => 'Article',
+            'test' => 'Test',
+        ),
+    'configgroup' =>
+        array (
+            'basic' => 'Basic',
+            'money' => 'Money',
+            'email' => 'Email',
+            'dictionary' => 'Dictionary',
+            'user' => 'User',
+            'example' => 'Example',
+        ),
+    'mail_type' => '1',
+    'mail_smtp_host' => 'smtp.qq.com',
+    'mail_smtp_port' => '465',
+    'mail_smtp_user' => '10000',
+    'mail_smtp_pass' => 'password',
+    'mail_verify_type' => '2',
+    'mail_from' => '10000@qq.com',
+    'company_single_withdraw_max' => '500000',
+    'company_weekly_withdraw_amount' => '200000',
+    'elite_single_withdraw_max' => '400000',
+    'elite_weekly_withdraw_amount' => '200000',
 );
